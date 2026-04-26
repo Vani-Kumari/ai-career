7ea170b5a765f48ab96c50c1ac0c8d3eb075f7e3
vani-Kumari <vanikumari.225014@gmail.com>
2026-04-26 20:06:00 +0530

    implemented InputPanel for resume and job description input

--- frontend/src/components/InputPanel.jsx
+++ frontend/src/components/InputPanel.jsx
@@ -1,7 +1,28 @@
-
-
-export const InputPanel = () => {
+function InputPanel({ setResume, setJd, handleAnalyze, loading }) {
   return (
-    <div>InputPanel</div>
-  )
+    <div className="bg-white p-5 rounded-2xl shadow-md">
+      <h2 className="font-semibold mb-3">Resume</h2>
+
+      <textarea
+        className="w-full h-40 border p-3 rounded-lg mb-3"
+        onChange={(e) => setResume(e.target.value)}
+      />
+
+      <h2 className="font-semibold mb-3">Job Description</h2>
+
+      <textarea
+        className="w-full h-40 border p-3 rounded-lg"
+        onChange={(e) => setJd(e.target.value)}
+      />
+
+      <button
+        onClick={handleAnalyze}
+        className="w-full mt-4 bg-[#6366F1] text-white py-2 rounded-lg"
+      >
+        {loading ? "Analyzing..." : "Analyze Resume"}
+      </button>
+    </div>
+  );
 }
+
+export default InputPanel;
